778942e7a14644f03f564ad1f35a1e42ca63519f
Grzegorz <grzegorz.sikorski.96@gmail.com>
2019-10-09 18:46:00 +0200

    behat

---
+++ features/TestPath/test.feature
@@ -0,0 +1,5 @@
+@test
+Feature: test feature
+
+  Scenario: Test Scenario
+    Given user with email 'test@example.com' and username 'test'
---
+++ features/bootstrap/TestContext.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BehatTests;
+
+use Behat\Behat\Context\Context;
+use PHPUnit\Framework\Assert;
+
+/**
+ * Class TestContext
+ * @package BehatTests
+ */
+class TestContext implements Context
+{
+    /**
+     * @Given user with email :email and username :username
+     * @param string $email
+     * @param string $username
+     */
+    public function userWithEmailAndUsername($email, $username)
+    {
+        Assert::assertEquals($username . '@example.com', $email);
+    }
+}
